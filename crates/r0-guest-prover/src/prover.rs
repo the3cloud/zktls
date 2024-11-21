2f4d43cd5b239449093463ac60307234ab7bf1f8
steve-the3cloud <steve@the3.cloud>
2024-11-21 11:07:21 +0800

    add prover

---
+++ crates/r0-guest-prover/src/prover.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
+use t3zktls_core::{GuestInput, GuestOutput};
+
+pub struct Risc0GuestProver {}
+
+fn prover(input: GuestInput) -> Result<GuestOutput> {
+    let prover = default_prover();
+
+    let env = ExecutorEnv::builder()
+        .write(&input)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let prove_result = prover
+        .prove_with_opts(env, t3zktls_program::TLS_R0_ELF, &ProverOpts::groth16())
+        .unwrap();
+
+    let journal = prove_result.receipt.journal;
+    let guest_output: GuestOutput = journal.decode()?;
+
+    Ok(guest_output)
+}
