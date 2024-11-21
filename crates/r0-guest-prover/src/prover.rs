5bc3e3ec2ed7a45b88cdb870d672c332e684c7b6
steve-the3cloud <steve@the3.cloud>
2024-11-21 11:36:23 +0800

    add r0 prover support

--- crates/r0-guest-prover/src/prover.rs
+++ crates/r0-guest-prover/src/prover.rs
@@ -1,10 +1,22 @@
+use std::panic;
+
 use anyhow::Result;
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
-use t3zktls_core::{GuestInput, GuestOutput};
+use t3zktls_core::{GuestInput, GuestOutput, GuestProver};
 
 pub struct Risc0GuestProver {}
 
-fn prover(input: GuestInput) -> Result<GuestOutput> {
+impl GuestProver for Risc0GuestProver {
+    async fn prove(&mut self, guest_input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
+        panic_catched_prover(guest_input)
+    }
+}
+
+fn panic_catched_prover(input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
+    panic::catch_unwind(move || prover(input)).map_err(|e| anyhow::anyhow!("{:?}", e))?
+}
+
+fn prover(input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
     let prover = default_prover();
 
     let env = ExecutorEnv::builder()
@@ -14,13 +26,13 @@ fn prover(input: GuestInput) -> Result<GuestOutput> {
         .unwrap();
 
     let prove_result = prover
-        .prove_with_opts(env, t3zktls_program_r0
-            
-            ::TLS_R0_ELF, &ProverOpts::groth16())
+        .prove_with_opts(env, t3zktls_program_r0::TLS_R0_ELF, &ProverOpts::groth16())
         .unwrap();
 
     let journal = prove_result.receipt.journal;
     let guest_output: GuestOutput = journal.decode()?;
 
-    Ok(guest_output)
+    let proof = prove_result.receipt.inner.groth16()?.seal.clone();
+
+    Ok((guest_output, proof))
 }
