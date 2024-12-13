1b4246273c48f051c073aa779cc104ef6ff640f2
steve-the3cloud <steve@the3.cloud>
2024-12-13 23:54:57 +0800

    enable replayable-tls

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -3,10 +3,6 @@ use alloy::sol;
 sol! {
     #[sol(rpc)]
     interface IZkTLSDAppCallback {
-        /// @notice Callback function that receives the response from a ZkTLS request
-        /// @dev This function is called by the ZkTLS gatewal when a response is ready
-        /// @param requestId_ The unique identifier of the TLS request
-        /// @param response_ The verified response data from the TLS request
         function deliveryResponse(bytes32 requestId_, bytes calldata response_) external;
     }
 }
