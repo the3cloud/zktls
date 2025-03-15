eb3c878052ff231b83ac8b3f8220e1f0b618ba52
Your Name <you@example.com>
2025-03-15 22:56:38 +0800

    fix r0

--- crates/guest-prover-r0/src/prover.rs
+++ crates/guest-prover-r0/src/prover.rs
@@ -1,34 +1,48 @@
-use std::panic;
+use std::{future::Future, panic};
 
+use alloy_primitives::{hex, B256};
 use anyhow::Result;
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
-use t3zktls_core::{GuestInput, GuestOutput, GuestProver};
+use t3zktls_core::ZkProver;
+use zktls_program_core::{GuestInput, Response};
 
 #[derive(Default)]
 pub struct Risc0GuestProver {}
 
-impl GuestProver for Risc0GuestProver {
-    async fn prove(&mut self, guest_input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
-        panic_catched_prover(guest_input)
+impl ZkProver for Risc0GuestProver {
+    fn prove(
+        &mut self,
+        input: GuestInput,
+        _pvkey: B256,
+        guest_program: &[u8],
+    ) -> impl Future<Output = Result<Response>> + Send {
+        panic_catched_prover(input, guest_program)
     }
 }
 
-fn panic_catched_prover(input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
-    panic::catch_unwind(move || prover(input)).map_err(|e| anyhow::anyhow!("{:?}", e))?
+async fn panic_catched_prover(input: GuestInput, guest_program: &[u8]) -> Result<Response> {
+    panic::catch_unwind(move || prover(input, guest_program))
+        .map_err(|e| anyhow::anyhow!("{:?}", e))?
 }
 
-fn prover(input: GuestInput) -> Result<(GuestOutput, Vec<u8>)> {
+fn prover(input: GuestInput, guest_program: &[u8]) -> Result<Response> {
     let prover = default_prover();
 
     let env = ExecutorEnv::builder().write(&input)?.build()?;
 
-    let prove_result =
-        prover.prove_with_opts(env, t3zktls_program_r0::TLS_R0_ELF, &ProverOpts::groth16())?;
+    let prove_result = prover.prove_with_opts(env, guest_program, &ProverOpts::groth16())?;
 
     let journal = prove_result.receipt.journal;
-    let guest_output: GuestOutput = journal.decode()?;
+    let mut response: Response = journal.decode()?;
 
-    let proof = prove_result.receipt.inner.groth16()?.seal.clone();
+    let mut proof = prove_result.receipt.inner.groth16()?.seal.clone();
 
-    Ok((guest_output, proof))
+    log::debug!("proof: {}", hex::encode(&proof));
+
+    if proof.len() <= 4 {
+        proof = Vec::new();
+    }
+    response.proof = proof.into();
+
+    Ok(response)
 }
